850d1d5127b3b82542fb6a8bad740f3638c9e661
mac <mac@macdeMac-mini.local>
2025-08-31 04:10:31 +0800

    final

--- static/src/js/pos_update_rewards.js
+++ static/src/js/pos_update_rewards.js
@@ -4,6 +4,12 @@ odoo.define('pos_loyalty_updating_reward.pos_polling', function (require) {
     const { Order } = require('point_of_sale.models');
     const Registries = require('point_of_sale.Registries');
     const { qweb, _t }= require('web.core');
+    const { round_decimals,round_precision } = require('web.utils');
+
+    function _newRandomRewardCode() {
+        return (Math.random() + 1).toString(36).substring(3);
+    }
+
     const KeepUpdatingReward = (Order) =>
         class extends Order {
             constructor() {
@@ -31,12 +37,20 @@ odoo.define('pos_loyalty_updating_reward.pos_polling', function (require) {
             }
             args = args || {};
             if (!args['product']){
-                 console.log("i am here")
-                 var rw = this.pos.reward_by_id[reward.id]
-                 console.log(rw)
-                 console.log(rw.all_discount_product_ids.values().next().value)
-                 args['product'] = this.pos.db.get_product_by_id(rw.all_discount_product_ids.values().next().value);
-                 console.log(args['product'])
+                if(reward.reward_type == "product"){
+                    var rw = this.pos.reward_by_id[reward.id]
+                    console.log(rw)
+                    console.log(rw.reward_product_ids[0])
+                    args['product'] = this.pos.db.get_product_by_id(rw.reward_product_ids[0]);
+                }else{
+                    console.log("i am here")
+                    var rw = this.pos.reward_by_id[reward.id]
+                    console.log(rw)
+                    console.log(rw.all_discount_product_ids.values().next().value)
+                    args['product'] = this.pos.db.get_product_by_id(rw.all_discount_product_ids.values().next().value);
+                    console.log(args['product'])
+                }
+                 
             }
             if(args['product']){
                     const rewardLines = this._getRewardLineValues({
@@ -44,7 +58,7 @@ odoo.define('pos_loyalty_updating_reward.pos_polling', function (require) {
                     coupon_id: coupon_id,
                     product: args['product'] || null,
                     });
-                    if(rewardLines && rewardLines[0] && !rewardLines[0]['product']){
+                    if(rewardLines && rewardLines[0] && !rewardLines[0]['product'] && typeof args['product'] !== 'string' && typeof args['product'] !== 'number' ){
                         console.log("working working working working :) :)")
                         rewardLines[0]['product'] = args['product']
                     }
@@ -80,6 +94,158 @@ odoo.define('pos_loyalty_updating_reward.pos_polling', function (require) {
         return true;
        
     }
+
+    //   _programIsApplicable(program) {
+    //     if(program && program.rules){
+    //         if (program.trigger === 'auto' && !program.rules.find((rule) => rule.mode === 'auto' || this.codeActivatedProgramRules.includes(rule.id))) {
+    //             return false;
+    //         }
+    //         if (program.trigger === 'with_code' && !program.rules.find((rule) => this.codeActivatedProgramRules.includes(rule.id))) {
+    //             return false;
+    //         }
+    //         if (program.is_nominative && !this.get_partner()) {
+    //             return false;
+    //         }
+    //         if (program.date_to && program.date_to <= new Date()) {
+    //             return false;
+    //         }
+    //         if (program.limit_usage && program.total_order_count >= program.max_usage) {
+    //             return false;
+    //         }
+    //          return true;
+    //     }
+    //     return true
+       
+       
+    // }
+
+    //  _getRealCouponPoints(coupon_id) {
+    //     let points = 0;
+    //     const dbCoupon = this.pos.couponCache[coupon_id];
+    //     if (dbCoupon) {
+    //         points += dbCoupon.balance;
+    //     }
+    //     Object.values(this.couponPointChanges).some((pe) => {
+    //         if (pe.coupon_id === coupon_id) {
+    //             if(this.pos.program_by_id[pe.program_id]){
+    //                 if (this.pos.program_by_id[pe.program_id].applies_on !== 'future') {
+    //                     points += pe.points;
+    //                 }
+    //                 // couponPointChanges is not supposed to have a coupon multiple times
+    //                 return true;
+    //             }
+                
+    //         }
+    //         return false
+    //     });
+    //     for (const line of this.get_orderlines()) {
+    //         if (line.is_reward_line && line.coupon_id === coupon_id) {
+    //             points -= line.points_cost;
+    //         }
+    //     }
+    //     return points
+    // }
+
+
+    // _computeUnclaimedFreeProductQty(reward, coupon_id, product, remainingPoints) {
+    //         console.log("next step ....")
+    //         console.log(product)
+    //         if (!product){
+    //             return
+    //         }
+    //         let claimed = 0;
+    //         let available = 0;
+    //         let shouldCorrectRemainingPoints = false;
+    //         for (const line of this.get_orderlines()) {
+    //             if (reward.reward_product_ids.includes(product.id) && reward.reward_product_ids.includes(line.product.id)) {
+    //                 if (this._get_reward_lines() == 0) {
+    //                     if (line.get_product().id === product.id) {
+    //                         available += line.get_quantity();
+    //                     }
+    //                 } else {
+    //                     available += line.get_quantity();
+    //                 }
+    //             } else if (reward.reward_product_ids.includes(line.reward_product_id)) {
+    //                 if (line.reward_id == reward.id ) {
+    //                     remainingPoints += line.points_cost;
+    //                     claimed += line.get_quantity();
+    //                 } else {
+    //                     shouldCorrectRemainingPoints = true;
+    //                 }
+    //             }
+    //         }
+    //         let freeQty;
+    //         if (reward.program_id.trigger == 'auto') {
+    //             if (this._isRewardProductPartOfRules(reward, product) && reward.program_id.applies_on !== 'future') {
+    //                 // OPTIMIZATION: Pre-calculate the factors for each reward-product combination during the loading.
+    //                 // For points not based on quantity, need to normalize the points to compute free quantity.
+    //                 const appliedRulesIds = this.couponPointChanges[coupon_id].appliedRules;
+    //                 const appliedRules = appliedRulesIds !== undefined
+    //                     ? reward.program_id.rules.filter(rule => appliedRulesIds.includes(rule.id))
+    //                     : reward.program_id.rules;
+    //                 let factor = 0;
+    //                 let orderPoints = 0;
+    //                 for (const rule of appliedRules) {
+    //                     if (rule.any_product || rule.valid_product_ids.has(product.id)) {
+    //                         if (rule.reward_point_mode === 'order') {
+    //                             orderPoints += rule.reward_point_amount;
+    //                         } else if (rule.reward_point_mode === 'money') {
+    //                             factor += round_precision(rule.reward_point_amount * product.lst_price, 0.01);
+    //                         } else if (rule.reward_point_mode === 'unit') {
+    //                             factor += rule.reward_point_amount;
+    //                         }
+    //                     }
+    //                 }
+    //                 if (factor === 0) {
+    //                     freeQty = Math.floor((remainingPoints / reward.required_points) * reward.reward_product_qty);
+    //                 } else {
+    //                     const correction = shouldCorrectRemainingPoints ? this._getPointsCorrection(reward.program_id) : 0
+    //                     freeQty = computeFreeQuantity((remainingPoints - correction - orderPoints) / factor, reward.required_points / factor, reward.reward_product_qty);
+    //                     freeQty += Math.floor((orderPoints / reward.required_points) * reward.reward_product_qty);
+    //                 }
+    //             } else {
+    //                 freeQty = Math.floor((remainingPoints / reward.required_points) * reward.reward_product_qty);
+    //             }
+    //         } else if (reward.program_id.trigger == 'with_code') {
+    //             freeQty = Math.floor((remainingPoints / reward.required_points) * reward.reward_product_qty);
+    //         }
+    //         return Math.min(available, freeQty) - claimed;
+    //     }
+
+
+    //      _getRewardLineValuesProduct(args) {
+    //             const reward = args['reward'];
+    //             const product = this.pos.db.get_product_by_id(args['product'] || reward.reward_product_ids[0]);
+    //             if(!product){
+    //                 return []
+    //             }
+    //             const points = this._getRealCouponPoints(args['coupon_id']);
+    //             const unclaimedQty = this._computeUnclaimedFreeProductQty(reward, args['coupon_id'], product, points);
+    //             if (unclaimedQty <= 0) {
+    //                 return _t("There are not enough products in the basket to claim this reward.");
+    //             }
+    //             const claimable_count = reward.clear_wallet ? 1 : Math.min(Math.ceil(unclaimedQty / reward.reward_product_qty), Math.floor(points / reward.required_points));
+    //             const cost = reward.clear_wallet ? points : claimable_count * reward.required_points;
+    //             // In case the reward is the product multiple times, give it as many times as possible
+    //             const freeQuantity = Math.min(unclaimedQty, reward.reward_product_qty * claimable_count);
+    //             return [{
+    //                 product: reward.discount_line_product_id,
+    //                 price: -round_decimals(product.get_price(this.pricelist, freeQuantity), this.pos.currency.decimal_places),
+    //                 tax_ids: product.taxes_id,
+    //                 quantity: freeQuantity,
+    //                 reward_id: reward.id,
+    //                 is_reward_line: true,
+    //                 reward_product_id: product.id,
+    //                 coupon_id: args['coupon_id'],
+    //                 points_cost: cost,
+    //                 reward_identifier_code: _newRandomRewardCode(),
+    //                 merge: false,
+    //             }]
+    //         }
+            
+
+
+    
 
 
 
